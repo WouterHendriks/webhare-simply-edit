0f03f078bf7e3e7fd3defd4fe00772b9e717d61d
Arnold Hendriks <a.hendriks@b-lex.nl>
2015-09-19 20:04:57 +0200

    First designfiles

---
+++ webdesigns/first/web/js/first.js
@@ -0,0 +1,8 @@
+require ('../css/first.scss');
+
+function init()
+{
+  console.log("Hi, this is first.js - DOMContentLoaded just triggered");
+}
+
+document.addEventListener("DOMContentLoaded", init);
